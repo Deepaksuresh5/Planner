fb6c91dbda5819e7ff1c58039f5e904ee24742a5
Deepaksuresh5 <deepak.suresh5@gmail.com>
2026-06-15 13:06:56 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE='planner-v2';
-const ASSETS=['/','/index.html','/manifest.json','/icons/icon-192.png','/icons/icon-512.png'];
+const CACHE='planner-v3';
+const ASSETS=[
+  'https://deepaksuresh5.github.io/planner/',
+  'https://deepaksuresh5.github.io/planner/index.html',
+  'https://deepaksuresh5.github.io/planner/manifest.json',
+  'https://deepaksuresh5.github.io/planner/icons/icon-192.png',
+  'https://deepaksuresh5.github.io/planner/icons/icon-512.png'
+];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
   self.skipWaiting();
